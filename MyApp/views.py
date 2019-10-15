68dad34c8cd7fdd66524f2de232016b09a10f862
Xhendor <rosendorsc@gmail.com>
2019-10-14 19:57:51 -0700

    Se agrego bootrstrap y modelo a vista Revenge

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from array import *
 from django.http import HttpResponse
 
+from .models import Board
 
 def index(request):
-    return render(request, 'paginaRevenge.html',{})
+    data = Board.objects.all()
+    return render(request, 'paginaRevenge.html',{'posts': data})
 
 def otra(request):
     return render(request, 'otra.html',{})
